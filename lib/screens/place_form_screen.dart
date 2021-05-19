e6fcd94ae48988114c082af804d2e7ea6ac03eda
Gonzaga <luizgonzagabn@gmail.com>
2021-05-19 16:58:23 -0300

    commit

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:great_places/widgets/image_input.dart';
 
@@ -8,6 +10,11 @@ class PlaceFormScreen extends StatefulWidget {
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
   final _titleController = TextEditingController();
+  File? _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
 
   void _submitForm() {}
 
@@ -33,7 +40,7 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
                       ),
                     ),
                     SizedBox(height: 10),
-                    ImageInput(),
+                    ImageInput(this._selectImage),
                   ],
                 ),
               ),
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,11 +1,36 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+  ImageInput(this.onSelectImage);
+
   @override
   _ImageInputState createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  _takePicture() async {
+    final ImagePicker _picker = ImagePicker();
+    PickedFile? imageFile = await _picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (imageFile == null) return;
+
+    setState(() {
+      _storedImage = File(imageFile!.path);
+    });
+
+    widget.onSelectImage(...);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -17,14 +42,20 @@ class _ImageInputState extends State<ImageInput> {
             border: Border.all(width: 1, color: Colors.grey),
           ),
           alignment: Alignment.center,
-          child: Text('Nenhuma imagem!'),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : Text('Nenhuma imagem!'),
         ),
         SizedBox(width: 10),
         Expanded(
           child: TextButton.icon(
             icon: Icon(Icons.camera),
             label: Text('Tirar Foto'),
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
