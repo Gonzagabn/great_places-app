5340334525e8c7c34a809a2353182d9eedd9a41d
Gonzaga <luizgonzagabn@gmail.com>
2021-05-20 10:39:15 -0300

    commit

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _previewImageUrl == null
+              ? Text('Localização não informada!')
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+      ],
+    );
+  }
+}
