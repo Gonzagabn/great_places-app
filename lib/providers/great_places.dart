6bab1430f2820905f14f2541ad69109f89519086
Gonzaga <luizgonzagabn@gmail.com>
2021-05-20 10:04:00 -0300

    commit

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:great_places/models/place.dart';
+import 'package:great_places/utils/db_util.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -28,6 +29,11 @@ class GreatPlaces with ChangeNotifier {
     );
 
     _items.add(newPlace);
+    DbUtil.insert('places', {
+      'id': newPlace.id!,
+      'title': newPlace.title!,
+      'image': newPlace.image!.path,
+    });
     notifyListeners();
   }
 }
