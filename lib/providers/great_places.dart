6b4507f88bdc6e990e7f58b1d9d4c6e1d4006afe
Gonzaga <luizgonzagabn@gmail.com>
2021-05-20 10:24:50 -0300

    commit

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -8,6 +8,21 @@ import 'package:great_places/utils/db_util.dart';
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
 
+  Future<void> loadPlaces() async {
+    final dataList = await DbUtil.getData('places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
+    notifyListeners();
+  }
+
   List<Place> get items {
     return [..._items];
   }
